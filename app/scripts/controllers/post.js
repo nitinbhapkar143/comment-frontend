6be2a48e362633128a3a296f38e807eab558c978
nitin <nitin@capitalquotient.com>
2019-10-16 14:54:45 +0530

    fix comment sorting and validations

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -37,13 +37,18 @@ angular.module('commentsApp')
     }
 
     $scope.addComment = () => {
+      if(!$scope.comment || !$scope.comment.text) {
+        $scope.errorMessage = "Please enter text."
+        return
+      }
+      $scope.errorMessage = false;
       const data = {
         text: $scope.comment.text,
         post_id: $scope.post.id,
       }
       commentsService.addComment(data).then(comment => {
         if (comment.status) {
-          $scope.comments.push(comment.comment);
+          $scope.comments.unshift(comment.comment);
           $scope.comment = {}
         } else {
           $scope.errorMessage = comment.message;
@@ -53,23 +58,45 @@ angular.module('commentsApp')
 
     $scope.submitEdit = index => {
       if ($scope.type == "edit") {
+        if(!$scope.comments[index] || !$scope.comments[index].editText) {
+          $scope.comments[index].errorMessage = "Please enter text.";
+          return
+        }
         const data = Object.assign({}, $scope.comments[index]);
         data.text = data.editText;
         commentsService.editComment(data);
         $scope.comments[index].isEditing = false;
         $scope.comments[index].text = $scope.comments[index].editText;
       } else {
+        if(!$scope.comments[index] || !$scope.comments[index].editText) {
+          $scope.comments[index].errorMessage = "Please enter text.";
+          return
+        }
+        $scope.comments[index].errorMessage = false;
         const data = Object.assign({}, $scope.comments[index]);
         data.text = data.editText;
         data.parent_id = data.id;
         commentsService.addComment(data).then(comment => {
           if (comment.status) {
-            $scope.comments.splice(index + 1, 0, comment.comment);
+            let i = index;
+            if($scope.comments[index].parent_id != null){
+              while(i < $scope.comments.length && $scope.comments[i].parent_id != null && $scope.comments[index].parent_id <= $scope.comments[i].parent_id){
+                i++;
+              }
+            }else{
+              i++;
+              while(i < $scope.comments.length && $scope.comments[i].parent_id != null ){
+                i++;
+              }
+            }
+            if(i >= $scope.comments.length) i = $scope.comments.length - 1;
+            else if($scope.comments[i].parent_id == null || $scope.comments[index].parent_id > $scope.comments[i].parent_id) i--;
+            $scope.comments.splice(i + 1, 0, comment.comment);
+            $scope.comments[index].isEditing = false;
           } else {
-            $scope.errorMessage = comment.message;
+            $scope.comments[index].errorMessage = comment.message;
           }
         })
-        $scope.comments[index].isEditing = false;
       }
     }
 
